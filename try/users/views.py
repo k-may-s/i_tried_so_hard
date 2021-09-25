5c34b3554a0aaa2dbdb3ebb91e3be10d365dd37e
Ksenia <kseniya.mayach@yandex.ru>
2021-09-25 19:38:33 +0500

    Сделала обработку ошибки 204, починила update

--- try/users/views.py
+++ try/users/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import get_object_or_404, redirect
+from django.http import HttpResponse
 from .models import Client
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -27,16 +26,16 @@ def details_client_view(request, id):
     try:
         client_data = Client.objects.values().get(id=id)
     except Exception:
-        raise Http404('Client not found')
+        return HttpResponse(status=204)
     return Response({"data": client_data})
 
 
 @api_view(['POST'])
 def delete_client(request, id):
     try:
-        data = get_object_or_404(Client, id=id)
+        data = Client.objects.get(id=id)
     except Exception:
-        raise Http404('Client not found')
+        return HttpResponse(status=204)
     if request.method == 'POST':
         data.delete()
         return Response({"data": "Client was deleted"})
@@ -44,21 +43,21 @@ def delete_client(request, id):
         return Response({"data": "Client was not deleted"})
 
 
-@api_view(['GET', 'POST'])
+@api_view(['POST', 'GET'])
 def update_client_details(request, id):
     try:
-        client = Client.objects.values().get(id=id)
         if request.method == "POST":
-            client.last_name = request.POST.get("last_name")
-            client.name = request.POST.get("name")
-            client.middle_name = request.POST.get("middle_name")
-            client.phone_number = request.POST.get("phone_number")
-            client.address = request.POST.get("address")
-            client.rate = request.POST.get("rate")
+            client = Client.objects.get(id=id)
+            client.last_name = request.data.get("last_name")
+            client.name = request.data.get("name")
+            client.middle_name = request.data.get("middle_name")
+            client.phone_number = request.data.get("phone_number")
+            client.address = request.data.get("address")
+            client.rate = request.data.get("rate")
             client.save()
-            return redirect("<int:id>/")
+            return Response({"data": "Client data was updated"})
         else:
+            client = Client.objects.values().get(id=id)
             return Response({"data": client})
     except Exception:
-        raise Http404('Client not found')
-# TODO: Не работает POST для обновления полей - выходит ошибка "detail": "Not found."
+        return HttpResponse(status=204)
